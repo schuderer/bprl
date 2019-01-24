915d4644414eef41b2e2f36bb4f018830d62cc96
Andreas Schuderer <schuderer@gmx.de>
2019-01-24 13:27:36 +0100

    a little refactoring of q-learner

--- q_test_frozenlake.py
+++ q_test_frozenlake.py
@@ -5,34 +5,7 @@ import gym
 import random
 import numpy as np
 
-# Some primitive discretization helpers
-# Same-size bins:
-def discretizeSimple(val, min, max, numBins):
-    binWidth = (max-min) / numBins
-    return int((val-min) / binWidth)
-
-# Exponential binning helper:
-def discretizeExpPositive(val, min, max, bins):
-    range = max - min
-    valRel = val - min
-    currBin = bins - 1
-    currBorder = range / 2.0
-    while valRel < currBorder and currBin > 0:
-      currBin -= 1
-      currBorder = currBorder / 2.0
-    return currBin
-
-# Exponential binning (furthest bin = furthest half, next closest bin = furthest half of closer half, etc.)
-def discretizeExp(val, center, max, bins):
-    #if (max < 0 or min < 0 or bins < 1):
-    #  raise IllegalArgumentError("min and max must be 0.0 or greater, bins must be 1 or greater")
-    relVal = abs(val) - center
-    if val >= center:
-      return discretizeExpPositive(relVal, 0, max-center, bins)
-    else:
-      return -discretizeExpPositive(abs(relVal), 0, max-center, bins)
-
-
+# todo: same as just log2-transforming, then splitting and exp2-ing?? Then maybe can handle bi-directional (0-centered) exp-splits...
 def createExpSplits(high, bins, inverse):
     result = []
     range = high
@@ -51,8 +24,8 @@ def createExpGrid(highs, bins, inverse=False):
     return np.array([createExpSplits(h, b, inverse) for h,b in zip(highs,bins)])
 
 testGrid = createExpGrid([256, 1024], np.repeat([8],2))
-#print(testGrid)
-#print(discretizeExp(-3, 0, 1000, 10))
+print("testGrid:")
+print(testGrid)
 
 def discretize(vals, grid):
     # from https://github.com/udacity/deep-reinforcement-learning/blob/master/discretization/Discretization_Solution.ipynb
@@ -65,11 +38,14 @@ def undiscretize(indices, grid):
     return list(g[int(i)] for i, g in zip(indices, grid))
 
 
-#print(discretize([40, 12], testGrid))
+print(discretize([40, 12], testGrid))
+print("end of test")
 
 #env = PensionEnv()
 env = gym.make('FrozenLake-v0')
 
+state_grid = None
+statesDisc = None
 print("State space", env.observation_space)
 if type(env.observation_space) is gym.spaces.box.Box:
     discreteStates = False
@@ -85,8 +61,10 @@ elif type(env.observation_space) is gym.spaces.discrete.Discrete:
     discreteStates = True
     statesDisc = env.observation_space
 else:
-    raise NotImplementedError()
+    raise NotImplementedError("Can only work with Discrete or Box type state spaces.")
 
+action_grid = None
+actionsDisc = None
 print("Action space:", env.action_space)
 if type(env.action_space) is gym.spaces.box.Box:
     discreteActions = False
@@ -102,7 +80,7 @@ elif type(env.action_space) is gym.spaces.discrete.Discrete:
     discreteActions = True
     actionsDisc = env.action_space
 else:
-    raise NotImplementedError()
+    raise NotImplementedError("Can only work with Discrete or Box type action spaces.")
 
 #disc = discretize([35, 1000, 10000], state_grid)
 #print(disc)
@@ -113,7 +91,7 @@ def stateKeyFor(discreteObs):
         discreteObs = np.reshape(discreteObs, (1,))
     return "-".join([str(elem) for elem in discreteObs])
 
-def maxQ(discreteObs, defaultAction=0):
+def maxQ(discreteObs, qTable, defaultAction=0):
     if discreteObs.shape == ():
         discreteObs = np.reshape(discreteObs, (1,))
     highestVal = -np.inf
@@ -131,67 +109,69 @@ def maxQ(discreteObs, defaultAction=0):
         highestVal = 0.0
     return(bestActionIdx, highestVal)
 
-EPISODES = 1000
-MAX_STEPS = 100000
-
-STATE_BINS = 10
-ACTION_BINS = 10
-
-epsilon = 1.0 # exploration rate for e-greedy policy
-epsilon_decay = 0.995
-epsilon_min = 0.01
-alpha = 0.2 # temperature/learning rate
-gamma = 0.99 # discount factor
-
-qTable = {} # using dict instead of table, where key = state-action and value = value
-
-def tmpVisQTable():
+def print_q(qTable):
     for s in range(statesDisc.n):
         s = np.array(s)
         print([qTable.get(stateKeyFor(s) + "-" + str(a), 0) for a in range(actionsDisc.n)])
 
-for episode in range(EPISODES):
-    cumReward = 0
-    observation = env.reset()
-    #print("size of q table:", len(qTable.keys()))
-    print("epsilon:", epsilon)
-    for t in range(MAX_STEPS):
-        #env.render()
-        prev_obs = np.array(observation) if discreteStates else discretize(observation, state_grid) 
-        
-        # Select action according to epsilon-greedy policy
-        actionIdx = actionsDisc.sample()
-        if random.random() > epsilon: # greedy/argmax
-            actionIdx = maxQ(prev_obs, defaultAction=0)[0]
-        action = actionIdx if discreteActions else undiscretize([actionIdx], action_grid)[0]
-        #print("chosen action", actionIdx)
-        
-        # Take action
-        observation, reward, done, info = env.step(action)
-        curr_obs = np.array(observation) if discreteStates else discretize(observation, state_grid)
-        #print(maxQ(curr_obs))
-        
-        # Update the state that we acted on
-        stateActionKey = stateKeyFor(prev_obs)+"-"+str(actionIdx)
-        #print(stateActionKey)
-        
-        qValOld = qTable.get(stateActionKey, 0.0)
-        qValNew = qValOld + alpha*(reward + gamma*maxQ(curr_obs, defaultAction=actionsDisc.sample())[1] - qValOld)
-        #if qValOld != 0.0:
-        #    print(stateActionKey, qValOld, "<--", qValNew)
-        qTable[stateActionKey] = qValNew
-        
-        #h = info["human"]
-        #c = info["company"]
-        #print(info["year"], "human:", h.id, h.age, h.funds, h.lastTransaction, h.happiness, "reward:", reward, "company:", c.funds, c.reputation)
-        cumReward += reward
-        if done:
-            print("Episode {} finished after {} timesteps with cumulative reward {}".format(episode, t+1, cumReward))
-            epsilon = max(epsilon * epsilon_decay, epsilon_min)
-            #tmpVisQTable()
-            break
+def q_learn(env, alpha=0.2, gamma=0.99, epsilon=0.1, epsilon_decay=1.0, epsilon_min=0.0, episodes=100, max_steps=1000, q_table={}):
+    '''Generic Q-Learning algorithm. Returns the Q-Table.'''
+    # todo make self-contained
+    for episode in range(episodes):
+        cumReward = 0
+        observation = env.reset()
+        #print("size of q table:", len(q_table.keys()))
+        print("epsilon:", epsilon)
+        for t in range(max_steps):
+            #env.render()
+            prev_obs = np.array(observation) if discreteStates else discretize(observation, state_grid) 
+            
+            # Select action according to epsilon-greedy policy
+            actionIdx = actionsDisc.sample() # random
+            if random.random() > epsilon: # greedy/argmax
+                actionIdx = maxQ(prev_obs, q_table, defaultAction=0)[0]
+            action = actionIdx if discreteActions else undiscretize([actionIdx], action_grid)[0]
+            #print("chosen action", actionIdx)
+            
+            # Take action
+            observation, reward, done, info = env.step(action)
+            curr_obs = np.array(observation) if discreteStates else discretize(observation, state_grid)
+            #print(maxQ(curr_obs, q_table))
+            
+            # Update the state that we acted on
+            stateActionKey = stateKeyFor(prev_obs)+"-"+str(actionIdx)
+            #print(stateActionKey)
+            
+            qValOld = q_table.get(stateActionKey, 0.0)
+            qValNew = qValOld + alpha*(reward + gamma*maxQ(curr_obs, q_table)[1] - qValOld)
+            #if qValOld != 0.0:
+            #    print(stateActionKey, qValOld, "<--", qValNew)
+            q_table[stateActionKey] = qValNew
+            
+            #h = info["human"]
+            #c = info["company"]
+            #print(info["year"], "human:", h.id, h.age, h.funds, h.lastTransaction, h.happiness, "reward:", reward, "company:", c.funds, c.reputation)
+            cumReward += reward
+            if done:
+                print("Episode {} finished after {} timesteps with cumulative reward {}".format(episode, t+1, cumReward))
+                epsilon = max(epsilon * epsilon_decay, epsilon_min)
+                break
+    return q_table
 
-tmpVisQTable()
+
+# Run Q-Learning
+
+qTable = q_learn(env,
+                 alpha=0.2,   # temperature/learning rate
+                 gamma=0.99,  # discount factor
+                 epsilon=1.0, # initial exploration rate for e-greedy policy
+                 epsilon_decay=0.995, # epsilon reduction factor per episode
+                 epsilon_min=0.01,    # minimal epsilon
+                 episodes=1000,
+                 max_steps=10000, # abort episode after this number of steps
+                 q_table={})
+
+print_q(qTable)
 
 #(year, "human:", i, h.age, fundsBefore, h.funds, h.happiness, "reward:", r, "company:", companies[0].funds, companies[0].reputation)
 
