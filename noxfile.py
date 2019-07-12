f0202d171ed5c116f0bc3b3cc02b584821e39bfc
Andreas Schuderer <schuderer@gmx.de>
2019-07-12 20:56:31 +0200

    CI: Run coverall, try to get 2.7 working

--- noxfile.py
+++ noxfile.py
@@ -6,6 +6,7 @@ import nox
 # https://github.com/theacodes/nox/blob/master/noxfile.py
 
 os.environ["RELAX_IMPORTS"] = "true"
+ON_TRAVIS_CI = os.environ.get("TRAVIS")
 
 package_name = "gym_fin"
 my_py_ver = "3.7"
@@ -39,6 +40,13 @@ def install_requirements(session, dev=True, safety_check=True):
 
 
 @nox.session(python=my_py_ver)
+def format(session):
+    """Run code reformatter"""
+    session.install("black")
+    session.run("black", "-l", max_line_length, *autoformat)
+
+
+@nox.session(python=my_py_ver)
 def lint(session):
     """Run code style checker"""
     session.install("flake8", "flake8-import-order", "black")
@@ -52,17 +60,23 @@ def lint(session):
     )
 
 
-@nox.session(python=my_py_ver)
-def format(session):
-    """Run code reformatter"""
-    session.install("black")
-    session.run("black", "-l", max_line_length, *autoformat)
-
-
 # @nox.parametrize("django", ["1.9", "2.0"])
 # In Travis-CI: session selected via env vars
 @nox.session(python=["2.7", "3.5", "3.6", my_py_ver])
 def tests(session):
+    """Run the unit test suite"""
+    # already part of dev-Pipfile
+    # session.install('pytest', 'pytest-cov')
+    safety_check = "safety" in session.posargs
+    install_requirements(session, safety_check=safety_check)
+    # session.install('-e', '.')  # we're testing a package
+    # session.run('pipenv', 'install', '-e', '.')
+    pytest_args = ["pipenv", "run", "pytest", "tests", "--quiet"]
+    session.run(*pytest_args)
+
+
+@nox.session(python=my_py_ver)
+def coverage(session):
     """Run the unit test suite and check coverage"""
     # already part of dev-Pipfile
     # session.install('pytest', 'pytest-cov')
@@ -70,25 +84,24 @@ def tests(session):
     install_requirements(session, safety_check=safety_check)
     # session.install('-e', '.')  # we're testing a package
     # session.run('pipenv', 'install', '-e', '.')
+    pytest_args = ["pipenv", "run", "pytest", "tests", "--quiet"]
     session.run(
-        "pipenv",
-        "run",
-        "pytest",
-        "tests",
-        "--quiet",
+        *pytest_args,
         "--cov=" + package_name,
         "--cov=agents",
         "--cov-config",
         ".coveragerc",
-        "--cov-report=",
+        "--cov-report=",  # No coverage report
     )
-    session.install("coverage")
+    session.install("coverage", "coveralls")
     session.run(
         "coverage",
         "report",
         "--fail-under=3",  # TODO: get >= 90%
         "--show-missing",
     )
+    if ON_TRAVIS_CI:
+        session.run("coveralls")
     session.run("coverage", "erase")
 
 
