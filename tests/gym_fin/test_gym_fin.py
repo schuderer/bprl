880de70385370326ad4848ead218a95bd3a48b00
Andreas Schuderer <schuderer@gmx.de>
2020-08-16 12:07:08 +0200

    Add extensive unit tests for fin_base_sim, skip registration test

--- tests/gym_fin/test_gym_fin.py
+++ tests/gym_fin/test_gym_fin.py
@@ -1,6 +1,10 @@
 """Tests for the gym_fin.envs package"""
 
+import pytest
+
 import tests.utils as test_utils
+
+# from gym_fin.envs.sim_env import make_step, env_metadata
 
 # NOTE: import modules to test in the test functions, not here!
 
@@ -12,12 +16,22 @@ def test_no_do_profile_in_code():
         test_utils.import_submodules("gym_fin")
 
 
+@pytest.mark.skip(reason="registration mechanism is bound to change")
 def test_register():
-    # "gym_fin:FinBase-gym_fin.envs.sim_env.Entity.choose_some_action-v0"
     import gym
-    import gym_fin
 
-    _ = dir(gym_fin)  # avoid linting error
+    # class SomeClass():
+    #     @make_step(
+    #         observation_space=gym.spaces.Discrete(2),
+    #         observation_space_mapping=lambda self: 1,
+    #         action_space=gym.spaces.Discrete(2),
+    #         action_space_mapping={0: "A", 1: "B"},
+    #         reward_mapping=lambda self: 1,
+    #     )
+    #     def some_choice():
+    #         return "A"
+
+    # "gym_fin:FinBase-gym_fin.envs.sim_env.Entity.choose_some_action-v0"
 
     id_start = "FinBase-"
     env_ids = gym.envs.registry.env_specs.keys()
